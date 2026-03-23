dd2f52aa0a1a339e2d8d23bb4240a0f2f2dacfb9
yevdev016 <curaarvey@gmail.com>
2026-03-23 17:43:02 +0800

    Added header and hero sectio. Still under development

--- portfolio/src/components/common/Header.jsx
+++ portfolio/src/components/common/Header.jsx
@@ -4,6 +4,7 @@ import './Header.css';
 
 function Header() {
     const [isMenuActive, setIsMenuActive] = useState(false);
+
     function toggleMenu() {
         setIsMenuActive(!isMenuActive);
     }
@@ -15,13 +16,16 @@ function Header() {
             </div>
 
             <div className={`right-panel-container ${isMenuActive ? 'active' : ''}`}>
-                <Link to='/AboutMe'>About me</Link>
-                <Link to='/projects'>Projects</Link>
-                <Link to='/skills'>Skills</Link>
-                <Link to='/contact'>Contact</Link>
+                <Link to="/AboutMe" onClick={toggleMenu}>About me</Link>
+                <Link to="/projects" onClick={toggleMenu}>Projects</Link>
+                <Link to="/skills" onClick={toggleMenu}>Skills</Link>
+                <Link to="/contact" onClick={toggleMenu}>Contact</Link>
             </div>
 
-            <div className="hamburger-menu" onClick={toggleMenu}>
+            <div
+                className={`hamburger-menu ${isMenuActive ? 'active' : ''}`}
+                onClick={toggleMenu}
+            >
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
---
+++ portfolio/src/routes/portfolioRoute.tsx
@@ -0,0 +1,10 @@
+// import { Routes, Route } from 'react-router-dom';
+// function PortfolioRoute(){
+// <Routes>
+//   <Route path="/aboutMe" element={<AboutMe />} />
+//   <Route path="/projects" element={<Projects />} />
+//   <Route path="/skills" element={<Skills />} />
+//   <Route path="/contact" element={<Contact />} />
+// </Routes>
+// }
+// export default PortfolioRoute;
